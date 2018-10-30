40316f88100caa4c8eeff283721686ff73fb15c4
Jacob Mangia <tucker2134@gmail.com>
2018-10-30 17:18:15 -0400

    First Green State

---
+++ PencilDurabilityKataTests/Pencil/Pencil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PencilLibrary
+{
+    public class Pencil
+    {
+        public string write(string input)
+        {
+            return "Some Test Words";
+        }
+    }
+}
---
+++ PencilDurabilityKataTests/PencilDurabilityKataTests/PencilTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PencilLibrary;
+
+namespace PencilDurabilityKataTests
+{
+    [TestClass]
+    public class PencilTests
+    {
+        [TestMethod]
+        public void PencilAppendsTextToTheEndOfTheGivenString()
+        {
+            Pencil pencil = new Pencil();
+
+            Assert.AreEqual("Some Test Words", pencil.write("Some Test Words"));
+        }
+    }
+}
